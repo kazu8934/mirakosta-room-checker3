7b29e9048197ff29627d7c08d58be3d9f826dd6a
kazu8934 <tasuku765@gmail.com>
2025-07-14 22:11:21 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def check_rooms():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--remote-debugging-port=9222')
-    options.binary_location = '/usr/bin/chromium'  # VPS用に追加
+    options.binary_location = '/usr/bin/chromium-browser'  # ← ここがVPS専用
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
